62e37e84f9418fb7a7dc9c42d61d1d3f7f4fccb9
Tobias Viana <tobias.viana@usereserva.com>
2019-09-25 18:51:06 -0300

    CRUDzinho básico pra brincar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = require('../src/app')
+const app = require('./app')
 const config = require('./config')
 
-app.listen(config.APP_PORT, () => {
-  console.log('Listening on port', config.APP_PORT)
+app.listen(config.app.port, () => {
+  console.log('Listening on port', config.app.port)
 })
---
+++ seeds/users.js
@@ -0,0 +1,13 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('users')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { name: 'User 1', email: 'user1@company.com' },
+        { name: 'User 2', email: 'user2@company.com' },
+        { name: 'User 3', email: 'user3@company.com' }
+      ])
+    })
+}
